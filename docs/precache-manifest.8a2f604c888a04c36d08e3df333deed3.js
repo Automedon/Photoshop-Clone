008246cec6103ab0cb8f732a755ce06d6e368aa6
Automedon <42299107+Automedon@users.noreply.github.com>
2020-09-13 18:25:51 +0700

    home page :seat:

---
+++ docs/precache-manifest.8a2f604c888a04c36d08e3df333deed3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a18829cce06710ff9ecbc1697bc105",
+    "url": "/Photoshop-Clone/index.html"
+  },
+  {
+    "revision": "3af3d01ce1fd72708a30",
+    "url": "/Photoshop-Clone/static/css/main.6659a803.chunk.css"
+  },
+  {
+    "revision": "d1c21ad9fa0ab2c24f49",
+    "url": "/Photoshop-Clone/static/js/2.c2b1ef15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Photoshop-Clone/static/js/2.c2b1ef15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af3d01ce1fd72708a30",
+    "url": "/Photoshop-Clone/static/js/main.231f9003.chunk.js"
+  },
+  {
+    "revision": "4861b1cfab5c9a5058b6",
+    "url": "/Photoshop-Clone/static/js/runtime-main.c809fb56.js"
+  },
+  {
+    "revision": "81e0f40f45efa1205d562501acd73668",
+    "url": "/Photoshop-Clone/static/media/ptica-popugay-cvety-vetka-671.81e0f40f.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7996cec852ecb8c22b6a7085a69cbc3f.js"
+  "/Photoshop-Clone/precache-manifest.8a2f604c888a04c36d08e3df333deed3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Photoshop-Clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
